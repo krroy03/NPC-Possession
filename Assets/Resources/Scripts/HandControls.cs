99dc779d933773eb130a3c866b73f591a1e07c01
Chris Chi <chris4betterlife@gmail.com>
2016-03-24 18:04:26 -0400

    advanced hand control

--- Assets/Resources/Scripts/HandControls.cs
+++ Assets/Resources/Scripts/HandControls.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class HandControls : MonoBehaviour {
 	private int deviceIndex; 
 	private bool movingObj = false;
-	private Transform currentObj;
-	private Animator handAnimator;
+	private GameObject currentObj;
+	Animator handAnimator;
+	Vector3 speed;
+	Vector3 prePos;
 
 	public bool left;
 
@@ -16,25 +18,44 @@ public class HandControls : MonoBehaviour {
 		else
 			deviceIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost);
 
-		handAnimator = gameObject.GetComponent<Animator>();
+		handAnimator = gameObject.GetComponentInChildren<Animator>();
+
+		prePos = transform.position;
+		speed = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        MoveObject();
+        ReleaseObject();
 	}
 
-	void MoveObject() {
+	void FixedUpdate () {
+		Vector3 curPos = transform.position;
+		speed = (curPos - prePos) * (1 / Time.fixedDeltaTime);
+		prePos = curPos;
+	}
 
+	void MoveObject ()
+	{
+		if (left)
+			deviceIndex = SteamVR_Controller.GetDeviceIndex (SteamVR_Controller.DeviceRelation.Leftmost);
+		else
+			deviceIndex = SteamVR_Controller.GetDeviceIndex (SteamVR_Controller.DeviceRelation.Rightmost);
+		//Debug.Log("deviceIndex = " + deviceIndex);
 		if ((deviceIndex != -1 && SteamVR_Controller.Input (deviceIndex).GetPressDown (SteamVR_Controller.ButtonMask.Trigger))) {
-			handAnimator.SetBool("Fist",true);
-			handAnimator.SetBool("Idle",false);
+			handAnimator.SetBool ("Fist", true);
+			handAnimator.SetBool ("Idle", false);
 			if (!movingObj) {
-				Debug.Log("Trigger");
-				if (currentObj) {
-					currentObj.parent.SetParent (this.transform);
+				if (currentObj != null) {
+					currentObj.transform.parent = this.transform;
 					movingObj = true;
-
+					Rigidbody rg = currentObj.GetComponent<Rigidbody> ();
+					if (rg != null) {
+						currentObj.GetComponent<Rigidbody>().isKinematic = true;
+                    	currentObj.GetComponent<Rigidbody>().useGravity = false;
+					}
+                    
 				}
 					
 			}
@@ -42,12 +63,19 @@ public class HandControls : MonoBehaviour {
 		}
 	}
 
-	void ReleaseObject() {
+	void ReleaseObject ()
+	{
 		if ((deviceIndex != -1 && SteamVR_Controller.Input (deviceIndex).GetPressUp (SteamVR_Controller.ButtonMask.Trigger))) {
-			handAnimator.SetBool("Idle",true);
-			handAnimator.SetBool("Fist",false);
+			handAnimator.SetBool ("Idle", true);
+			handAnimator.SetBool ("Fist", false);
 			if (currentObj && movingObj) {
-				currentObj.parent.SetParent (null);
+				currentObj.transform.SetParent (null);
+				Rigidbody rg = currentObj.GetComponent<Rigidbody> ();
+				if (rg != null) {
+					rg.AddForce(speed);
+					currentObj.GetComponent<Rigidbody> ().isKinematic = false;
+					currentObj.GetComponent<Rigidbody> ().useGravity = true;
+				}
 				movingObj = false;
 				currentObj = null;
 			}
@@ -57,8 +85,7 @@ public class HandControls : MonoBehaviour {
 	void OnTriggerEnter(Collider col) {
 		if (!movingObj) {
 			if (col.gameObject.layer == 8) {
-				currentObj = col.gameObject.transform;
-			
+				currentObj = col.gameObject;
 			}
 		}
 	}
@@ -67,7 +94,6 @@ public class HandControls : MonoBehaviour {
 		if (!movingObj) {
 			if (col.gameObject.layer == 8) {
 				currentObj = null;
-
 			}
 		}
 	}
