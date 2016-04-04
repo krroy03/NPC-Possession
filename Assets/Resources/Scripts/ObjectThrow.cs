80f52cea5d14d3081108bd70928250d92f550b63
krroy03 <rikkyroy@gmail.com>
2016-04-04 18:56:07 -0400

    Trying to fix throwing to make it better
    Signed-off-by: krroy03 <rikkyroy@gmail.com>

--- Assets/Resources/Scripts/ObjectThrow.cs
+++ Assets/Resources/Scripts/ObjectThrow.cs
@@ -7,11 +7,18 @@ public class ObjectThrow : MonoBehaviour {
 	private Transform npcTransform = null;
 	public GameObject npc = null;
 
+	private GravityBody gBody; 
+
+
+	void Start() {
+		gBody = this.gameObject.GetComponent<GravityBody> ();
+	}
+
 	void FixedUpdate ()
 	{	
 		// when move the object, set the current player
+
 		if (GetComponent<Rigidbody> ().isKinematic == true) {
-			
 			cam = GameObject.Find("CamContainer");
 			npcTransform = cam.transform.GetChild(cam.transform.childCount - 1);
 			npc = npcTransform.gameObject;
