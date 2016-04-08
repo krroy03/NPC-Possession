215b8c2b4c310ca30d8fb0f6babef5c7f3d8cecb
Chris Chi <chris4betterlife@gmail.com>
2016-04-08 17:23:09 -0400

    soul attract
    Signed-off-by: Chris Chi <chris4betterlife@gmail.com>

---
+++ Assets/Resources/Scripts/Common/Bezier.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bezier : MonoBehaviour {
+
+	Vector3 p0;
+	Vector3 p1;
+	Vector3 p2;
+	Vector3 p3;
+	float ti = 0f;
+
+	Vector3 b0 = Vector3.zero;
+	Vector3 b1 = Vector3.zero;
+	Vector3 b2 = Vector3.zero;
+	Vector3 b3 = Vector3.zero;
+
+	float Ax, Ay, Az, Bx, By, Bz, Cx, Cy, Cz;
+
+	//Init function 
+	//v0 = 1st point v1 = handle of the 1st point v2 = handle of the 2nd point v3 = 2nd point
+
+	public void init(Vector3 v0, Vector3 v1, Vector3 v2, Vector3 v3)
+	{
+		this.p0 = v0;
+		this.p1 = v1;
+		this.p2 = v2;
+		this.p3 = v3;
+	}
+
+	// 0.0 >= t <= 1.0
+	public Vector3 GetPointAtTime (float t)
+	{
+		this.CheckConstant();
+		float t2 = t * t;
+		float t3 = t * t * t;
+		float x = this.Ax * t3 + this.Bx * t2 + this.Cx * t + p0.x;
+        float y = this.Ay * t3 + this.By * t2 + this.Cy * t + p0.y;
+        float z = this.Az * t3 + this.Bz * t2 + this.Cz * t + p0.z;
+        Vector3 vec = new Vector3(x,y,z);
+        return(vec);
+	}
+
+	void SetConstant() {
+        this.Cx = 3 * ((this.p0.x + this.p1.x) - this.p0.x);
+        this.Bx = 3 * ((this.p3.x + this.p2.x) - (this.p0.x + this.p1.x)) - this.Cx;
+        this.Ax = this.p3.x - this.p0.x - this.Cx - this.Bx;
+       
+        this.Cy = 3 * ((this.p0.y + this.p1.y) - this.p0.y);
+        this.By = 3 * ((this.p3.y + this.p2.y) - (this.p0.y + this.p1.y)) - this.Cy;
+        this.Ay = this.p3.y - this.p0.y - this.Cy - this.By;
+ 
+        this.Cz = 3 * ((this.p0.z + this.p1.z) - this.p0.z);
+        this.Bz = 3 * ((this.p3.z + this.p2.z) - (this.p0.z + this.p1.z)) - this.Cz;
+        this.Az = this.p3.z - this.p0.z - this.Cz - this.Bz;
+    }
+
+    //check if p0,p1,p2 or p3 have changed 
+	void CheckConstant() {
+        if (this.p0 != this.b0 || this.p1 != this.b1 || this.p2 != this.b2 || this.p3 != this.b3) {
+            this.SetConstant();
+            this.b0 = this.p0;
+            this.b1 = this.p1;
+            this.b2 = this.p2;
+            this.b3 = this.p3;
+        }
+    }
+
+}
--- Assets/Resources/Scripts/NPCCollision.cs
+++ Assets/Resources/Scripts/NPCCollision.cs
@@ -5,11 +5,15 @@ public class NPCCollision : MonoBehaviour {
 
 	GameObject NPC;
 	PossessionMechanic pm;
+	Vector3 start;
+	Vector3 end;
+	GameObject soul = null;
+	bool isCollide = false;
+
 	void Start ()
 	{
 		//get myself first
 		NPC = this.transform.parent.gameObject;
-
 		pm = GameObject.Find ("CamContainer").GetComponent<PossessionMechanic> ();
 	}
 
@@ -17,22 +21,54 @@ public class NPCCollision : MonoBehaviour {
 	void OnTriggerEnter (Collider col)
 	{
 		if (col.gameObject.tag == "Soul") {
-
+			soul = col.gameObject;
+			Debug.Log(soul);
 			ObjectThrow obj = col.gameObject.GetComponent<ObjectThrow> ();
 
-			//teleport to the NPC get hit if NPC I want to transfer is not myself
+			// teleport to the NPC get hit if NPC I want to transfer is not myself
 			// teleport to the NPC get hit if NPC I am transferring to is not myself 
 			// and also if obj is moving when it hits npc
-		
-			if (obj.GetComponent<Rigidbody> ().velocity.magnitude > 0.01f && !obj.GetComponent<Rigidbody> ().isKinematic && obj.npc && (obj.npc != NPC))   
+			//Test
+			obj.npc = GameObject.Find ("Roy");
+
+			//if (obj.GetComponent<Rigidbody> ().velocity.magnitude > 0.01f && !obj.GetComponent<Rigidbody> ().isKinematic && obj.npc && (obj.npc != NPC))   
 			{
+				isCollide = true;
+				end = obj.npc.transform.position;
+
+				/*
 				obj.GetComponent<Rigidbody> ().velocity = Vector3.zero;
 				pm.PossessNewNPC (NPC);
 
 				col.gameObject.GetComponent<SoulMovement> ().followHead = true;
 				col.gameObject.GetComponent<MeshRenderer> ().enabled = false;
+				*/
 			}
 		}
 	}
 
+	void OnTriggerExit (Collider col)
+	{
+		isCollide = false;
+	}
+
+	void Update ()
+	{
+		if (isCollide) {
+			soul.GetComponent<Rigidbody> ().AddForce (.1f * (end - soul.transform.position));
+			float distance = Vector3.Distance (end, soul.transform.position);
+			Debug.Log("enter");
+			Debug.Log("distance" + distance);
+			if(distance < 1f) {
+				Debug.Log("lalalal");
+				ObjectThrow obj = soul.GetComponent<ObjectThrow> ();
+				obj.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+				pm.PossessNewNPC (NPC);
+			    //soul.GetComponent<SoulMovement> ().followHead = true;
+				//soul.GetComponent<MeshRenderer> ().enabled = false;
+				isCollide = false;
+			}
+		}
+
+	}
 }
