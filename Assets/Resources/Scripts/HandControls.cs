c723c8b4ef1721ac262f2c29619d59bef6d100e9
Chris Chi <chris4betterlife@gmail.com>
2016-04-12 17:03:33 -0400

    delete the soul part/ add auto target  at dragon for testing

--- Assets/Resources/Scripts/HandControls.cs
+++ Assets/Resources/Scripts/HandControls.cs
@@ -10,22 +10,20 @@ public class HandControls : MonoBehaviour
 	Vector3 speed;
 	Vector3 prePos;
 	Vector3 curPos;
-
+	Vector3 dragonPosition = Vector3.zero;
 	public bool left;
-
-	public SoulMovement soul;
 
 	public GameObject controllerModel;
 	// Use this for initialization
 	void Start ()
-	{
+	{	
+		// check the left and right 
 		if (left)
 			deviceIndex = SteamVR_Controller.GetDeviceIndex (SteamVR_Controller.DeviceRelation.Leftmost);
 		else
 			deviceIndex = SteamVR_Controller.GetDeviceIndex (SteamVR_Controller.DeviceRelation.Rightmost);
-
+		// hand animator for grip and release 
 		handAnimator = gameObject.GetComponentInChildren<Animator> ();
-
 		prePos = this.transform.position;
 		speed = Vector3.zero;
 	}
@@ -37,97 +35,27 @@ public class HandControls : MonoBehaviour
 			deviceIndex = SteamVR_Controller.GetDeviceIndex (SteamVR_Controller.DeviceRelation.Leftmost);
 		else
 			deviceIndex = SteamVR_Controller.GetDeviceIndex (SteamVR_Controller.DeviceRelation.Rightmost);
-		
+		// throw object velocity 
 		curPos = this.transform.position;
-		speed = 100f * (curPos - prePos) * (1 / Time.deltaTime);
+		detectDragon();
+		if (dragonPosition != Vector3.zero) {
+			speed = 100f * (dragonPosition - prePos).Normalize() * (1 / Time.deltaTime); 
+			Debug.Log("enter");
+		} 
+		else {
+			speed = 100f * (curPos - prePos).Normalize() * (1 / Time.deltaTime);
+		}
 		prePos = curPos;
-
 		MoveObject ();
 		ReleaseObject ();
-		ReturnSoul ();
-	}
-
-
-
-	private bool triedToPickUp = false;
-	void MoveObject ()
-	{
-		if ((deviceIndex != -1 && SteamVR_Controller.Input (deviceIndex).GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) || Input.GetMouseButtonDown (0)) {
-			
-			//handAnimator.SetBool ("Fist", true);
-			//handAnimator.SetBool ("Idle", false);
-			if (!movingObj && currentObj) {
-				// change values for soul and objects
-				if (currentObj.tag == "Soul") {
-					currentObj.GetComponent<SoulMovement> ().followHead = false;
-					currentObj.GetComponent<MeshRenderer> ().enabled = true;
-				} else if (currentObj.layer == 8) {
-					currentObj.GetComponent<GravityBody> ().beingControlled = true;
-					// also change colors of object and make hand model invisible. 
-					currentObj.GetComponent<ObjectStats>().PickedUp();
-					controllerModel.SetActive (false);
-				}
-				// indicate that we are picking up an object
-				currentObj.transform.parent = this.transform;
-				movingObj = true;
-
-				Rigidbody rg = currentObj.GetComponent<Rigidbody> ();
-				if (rg != null) {
-					currentObj.GetComponent<Rigidbody> ().isKinematic = true;
-
-				}
-
-
-			}
-		}
-	}
-
-
-	private bool triedToRelease = false;
-	void ReleaseObject ()
-	{
-		if ((deviceIndex != -1 && SteamVR_Controller.Input (deviceIndex).GetPressUp (SteamVR_Controller.ButtonMask.Trigger)) || Input.GetMouseButtonUp (0)) {
-			
-			//handAnimator.SetBool ("Idle", true);
-			//handAnimator.SetBool ("Fist", false);
-			if (currentObj && movingObj) {
-				// change values for soul and objects
-				if (currentObj.tag == "Soul") {
-
-				} else if (currentObj.layer == 8) {
-					currentObj.GetComponent<GravityBody> ().beingControlled = false;
-					currentObj.GetComponent<GravityBody> ().planet = null;
-					controllerModel.SetActive (true);
-				}
-				// indicate that we have released an object
-				currentObj.transform.SetParent (null);
-				Rigidbody rg = currentObj.GetComponent<Rigidbody> (); 
-				if (rg != null) {
-					currentObj.GetComponent<Rigidbody> ().isKinematic = false;
-					rg.AddForce (speed);
-				}
-				movingObj = false;
-				currentObj = null;
-
-			}
-		}
-	}
-
-	// presss touchpad to return
-	void ReturnSoul ()
-	{
-		if ((deviceIndex != -1 && SteamVR_Controller.Input (deviceIndex).GetPressDown (SteamVR_Controller.ButtonMask.Touchpad))) {
-			//handAnimator.SetBool ("Idle", true);
-			//handAnimator.SetBool ("Fist", false);
-
-			soul.ReturnSoul ();
-		}
 	}
 
 	void OnTriggerEnter (Collider col)
-	{
+	{	
+		//not moving object and no object in hands
 		if (!movingObj && !currentObj) {
-			if (col.gameObject.layer == 8 || col.gameObject.tag == "Soul") {
+			// isObject
+			if (col.gameObject.layer == 8) {
 				ObjectThrow objThrow = col.gameObject.GetComponent<ObjectThrow> ();
 				if (!objThrow.touchingHand) {
 					// change color of object we are touching
@@ -145,6 +73,76 @@ public class HandControls : MonoBehaviour
 			}
 		}
 	}
+
+	void detectDragon ()
+	{
+		bool foundHit = false;
+		RaycastHit hit;
+		//cast a sphereray
+		foundHit = Physics.SphereCast (transform.position, 1, transform.forward, hit, 10);
+		if (foundHit) {
+			// if the ray hits a dragon
+			if (hit.collider.GetComponent<GameObject> ().tag == 11) {
+				Vector3 dragonPosition = hit.collider.GetComponent<GameObject>().transform;
+			}
+		}
+	}
+
+	void MoveObject ()
+	{	
+		// hasDevice && 
+		if ((deviceIndex != -1 && SteamVR_Controller.Input (deviceIndex).GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) || Input.GetMouseButtonDown (0)) {
+			
+			//handAnimator.SetBool ("Fist", true);
+			//handAnimator.SetBool ("Idle", false);
+			if (!movingObj && currentObj) {
+				// change values for soul and objects
+				if (currentObj.layer == 8) {
+					currentObj.GetComponent<GravityBody> ().beingControlled = true;
+					// also change colors of object and make hand model invisible. 
+					currentObj.GetComponent<ObjectStats> ().PickedUp ();
+					controllerModel.SetActive (false);
+				}
+				// indicate that we are picking up an object
+				currentObj.transform.parent = this.transform;
+				movingObj = true;
+				Rigidbody rg = currentObj.GetComponent<Rigidbody> ();
+				if (rg != null) {
+					currentObj.GetComponent<Rigidbody> ().isKinematic = true;
+				}
+			}
+		}
+	}
+
+	void ReleaseObject ()
+	{
+		if ((deviceIndex != -1 && SteamVR_Controller.Input (deviceIndex).GetPressUp (SteamVR_Controller.ButtonMask.Trigger)) || Input.GetMouseButtonUp (0)) {
+			
+			//handAnimator.SetBool ("Idle", true);
+			//handAnimator.SetBool ("Fist", false);
+			if (currentObj && movingObj) {
+				// change values for soul and objects
+				if (currentObj.layer == 8) {
+					currentObj.GetComponent<GravityBody> ().beingControlled = false;
+					currentObj.GetComponent<GravityBody> ().planet = null;
+					controllerModel.SetActive (true);
+				}
+				// indicate that we have released an object
+				currentObj.transform.SetParent (null);
+				Rigidbody rg = currentObj.GetComponent<Rigidbody> (); 
+				if (rg != null) {
+					currentObj.GetComponent<Rigidbody> ().isKinematic = false;
+					rg.AddForce (speed);
+					//reset 
+					dragonPosition = Vector3.zero;
+				}
+				movingObj = false;
+				currentObj = null;
+			}
+		}
+	}
+
+
 
 	void OnTriggerExit (Collider col)
 	{
