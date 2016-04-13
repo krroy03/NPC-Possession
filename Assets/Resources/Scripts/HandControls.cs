3a878290e246c8d697855b71d0743a358cb9577a
krroy03 <rikkyroy@gmail.com>
2016-04-12 23:27:15 -0400

    some fixes
    Signed-off-by: krroy03 <rikkyroy@gmail.com>

--- Assets/Resources/Scripts/HandControls.cs
+++ Assets/Resources/Scripts/HandControls.cs
@@ -31,13 +31,9 @@ public class HandControls : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if (left)
-			deviceIndex = SteamVR_Controller.GetDeviceIndex (SteamVR_Controller.DeviceRelation.Leftmost);
-		else
-			deviceIndex = SteamVR_Controller.GetDeviceIndex (SteamVR_Controller.DeviceRelation.Rightmost);
 		// throw object velocity 
 		curPos = this.transform.position;
-		speed = 50f * (curPos - prePos) / Time.deltaTime;
+		speed = 100f * (curPos - prePos) / Time.deltaTime;
 
 		MoveObject ();
 		ReleaseObject ();
@@ -75,14 +71,10 @@ public class HandControls : MonoBehaviour
 		//cast a sphereray
 		int mask = 1 << 11;
 		foundHit = Physics.SphereCast(transform.position, 20, (curPos - prePos).normalized ,out hit, 100, mask);
-		Debug.Log ("enter");
-		Debug.DrawRay (this.transform.position, (curPos - prePos).normalized * 100f, Color.magenta, 100, false);
+
 		if (foundHit) {
 			// if the ray hits a dragon
-			//if (hit.collider.gameObject.layer == 11) {
-				Debug.Log ("find");
 				dragonTransform = hit.collider.gameObject.transform;
-			//}
 		}
 	}
 
@@ -136,7 +128,7 @@ public class HandControls : MonoBehaviour
 					if (dragonTransform != null) {
 						Debug.Log ("findDragon");
 						currentObj.GetComponent<ObjectThrow> ().target = dragonTransform;
-						currentObj.GetComponent<ObjectThrow> ().speed = speed.magnitude * 50f;
+						currentObj.GetComponent<ObjectThrow> ().speed = speed.magnitude;
 						//reset 
 						dragonTransform = null;
 					} 
